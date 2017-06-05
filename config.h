ba32dfe38e84979d5ee0f67adfdda8e77d6a1eb7
Damon V. Caskey <dc@caskeys.com>
2017-06-05 16:18:57 -0400

    Config
    Initial config and macros.

---
+++ config.h
@@ -0,0 +1,21 @@
+#ifndef DC_SIDNEY
+#define DC_SIDNEY   1
+
+// Defaults.
+#define DC_SIDNEY_DEFAULT_SPEED_ADJUST          100
+#define DC_SIDNEY_DEFAULT_VOLUME_ADJUST_LEFT    200
+#define DC_SIDNEY_DEFAULT_VOLUME_ADJUST_MASTER  200
+#define DC_SIDNEY_DEFAULT_VOLUME_ADJUST_RIGHT   200
+
+// Keys
+#define DC_SIDNEY_KEY_INDEX                     "dcsidney_si"   // Sound index to play.
+#define DC_SIDNEY_KEY_PRIORITY                  "dcsidney_p"    // Priority of sound.
+#define DC_SIDNEY_KEY_SPEED_ADJUST              "dcsidney_sa"   // Speed to play sound.
+#define DC_SIDNEY_KEY_VOLUME_ADJUST_LEFT        "dcsidney_val"  // Volume adjustment, left channel.
+#define DC_SIDNEY_KEY_VOLUME_ADJUST_MASTER      "dcsidney_vam"  // Volume adjustment, general.
+#define DC_SIDNEY_KEY_VOLUME_ADJUST_RIGHT       "dcsidney_var"  // Volume adjustment, right channel.
+#define DC_SIDNEY_KEY_VOLUME_FINAL_LEFT         "dcsidney_vfl"  // Calculated volume, left.
+#define DC_SIDNEY_KEY_VOLUME_FINAL_MASTER       "dcsidney_vfm"  // Calculated volume, general.
+#define DC_SIDNEY_KEY_VOLUME_FINAL_RIGHT        "dcsidney_vfr"  // Calculated volume, right channel.
+
+#endif // DC_SIDNEY
